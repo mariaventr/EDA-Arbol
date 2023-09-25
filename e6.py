08dc0d27e39893d38921c9338c9b4f841f0c6196
mariaventr <128845946+mariaventr@users.noreply.github.com>
2023-09-25 16:56:58 -0300

    Update e6.py

--- e6.py
+++ e6.py
@@ -1,194 +1,185 @@
-class Item:
-    def __init__(self, key):
-        self.key = key
-        self.count = 1
-        self.p = None
+class NodoArbolB:
+    def __init__(self, hoja=True):
+        self.hoja = hoja
+        self.claves = []
+        self.hijos = []
 
-class Page:
-    def __init__(self):
-        self.m = 0
-        self.p0 = None
-        self.e = []
+class ArbolB:
+    def __init__(self, t):
+        self.raiz = NodoArbolB(True)
+        self.t = t
 
-n = 2
-raiz = None
-
-def binary_search(a, x):
-    l, r = 0, a.m - 1
-
-    while l <= r:
-        i = (l + r) // 2
-        if a.e[i].key < x:
-            l = i + 1
+    def insertar(self, k):
+        raiz = self.raiz
+        if len(raiz.claves) == (2 * self.t) - 1:
+            nuevo_nodo = NodoArbolB()
+            nuevo_nodo.hijos.append(raiz)
+            self.raiz = nuevo_nodo
+            self.dividir_nodo(nuevo_nodo, 0)
+            self.insertar_no_lleno(nuevo_nodo, k)
         else:
-            r = i - 1
+            self.insertar_no_lleno(raiz, k)
 
-    return l
-
-def insert(x, a):
-    global raiz
-    if a is None:
-        v = Item(x)
-        raiz = Page()
-        raiz.m = 1
-        raiz.e.append(v)
-        return
-
-    index = binary_search(a, x)
-
-    if index < a.m and a.e[index].key == x:
-        a.e[index].count += 1
-    else:
-        insert_page(x, a, index)
-
-def insert_page(x, a, index):
-    global raiz
-    v = Item(x)
-    if a.m < 2 * n:
-        a.e.insert(index, v)
-        a.m += 1
-    else:
-        split_page(x, a, index)
-
-def split_page(x, a, index):
-    global raiz
-    b = Page()
-    mid = n
-
-    if index <= n:
-        mid -= 1
-
-    b.e = a.e[mid + 1:]
-    a.e = a.e[:mid]
-    a.m = len(a.e)
-
-    if index <= n:
-        insert_page(x, a, index)
-    else:
-        insert_page(x, b, index - mid - 1)
-
-    if a is raiz:
-        raiz = Page()
-        raiz.m = 1
-        raiz.e.append(b)
-        raiz.p0 = a
-
-def search(x, a):
-    if a is None:
-        return None
-
-    index = binary_search(a, x)
-
-    if index < a.m and a.e[index].key == x:
-        return a.e[index]
-
-    if index <= a.m:
-        return search(x, a.e[index].p)
-
-def delete(x, a):
-    if a is None:
-        return
-
-    index = binary_search(a, x)
-
-    if index < a.m and a.e[index].key == x:
-        if a.e[index].count > 1:
-            a.e[index].count -= 1
+    def insertar_no_lleno(self, x, k):
+        i = len(x.claves) - 1
+        if x.hoja:
+            x.claves.append(0)
+            while i >= 0 and k < x.claves[i]:
+                x.claves[i + 1] = x.claves[i]
+                i -= 1
+            x.claves[i + 1] = k
         else:
-            delete_entry(x, a, index)
-    else:
-        delete(x, a.e[index].p)
+            while i >= 0 and k < x.claves[i]:
+                i -= 1
+            i += 1
+            if len(x.hijos[i].claves) == (2 * self.t) - 1:
+                self.dividir_nodo(x, i)
+                if k > x.claves[i]:
+                    i += 1
+            self.insertar_no_lleno(x.hijos[i], k)
 
-def delete_entry(x, a, index):
-    if a.m > n:
-        if index > 0:
-            a.e[index - 1].count -= 1
+    def dividir_nodo(self, x, i):
+        t = self.t
+        y = x.hijos[i]
+        z = NodoArbolB(y.hoja)
+        x.hijos.insert(i + 1, z)
+        x.claves.insert(i, y.claves[t - 1])
+        z.claves = y.claves[t:(2 * t) - 1]
+        y.claves = y.claves[0:(t - 1)]
+        if not y.hoja:
+            z.hijos = y.hijos[t:(2 * t)]
+            y.hijos = y.hijos[0:t]
+
+    def eliminar(self, k):
+        raiz = self.raiz
+        self.eliminar_clave(raiz, k)
+        if not raiz.claves and len(raiz.hijos) > 0:
+            self.raiz = raiz.hijos[0]
+
+    def eliminar_clave(self, x, k):
+        i = 0
+        while i < len(x.claves) and k > x.claves[i]:
+            i += 1
+        if i < len(x.claves) and k == x.claves[i]:
+            self.eliminar_clave_de_nodo(x, k, i)
+        elif not x.hoja:
+            self.eliminar_clave_de_no_hoja(x, k, i)
+        elif x.hoja:
+            print("La clave", k, "no está en el árbol")
+
+    def eliminar_clave_de_nodo(self, x, k, i):
+        if x.hoja:
+            del x.claves[i]
         else:
-            a.e[index + 1].count -= 1
-    else:
-        delete_merge(x, a, index)
+            k = x.claves[i]
+            if len(x.hijos[i].claves) >= self.t:
+                pred = self.get_predecesor(x.hijos[i])
+                x.claves[i] = pred
+                self.eliminar_clave(x.hijos[i], pred)
+            elif len(x.hijos[i + 1].claves) >= self.t:
+                succ = self.get_sucesor(x.hijos[i + 1])
+                x.claves[i] = succ
+                self.eliminar_clave(x.hijos[i + 1], succ)
+            else:
+                self.fusionar(x, i)
+                self.eliminar_clave(x.hijos[i], k)
 
-def delete_merge(x, a, index):
-    global raiz
-    if a is raiz:
-        if a.m == 1 and a.p0 is not None:
-            raiz = a.p0
-        return
+    def eliminar_clave_de_no_hoja(self, x, k, i):
+        t = self.t
+        if len(x.hijos[i].claves) < t:
+            if i > 0 and len(x.hijos[i - 1].claves) >= t:
+                self.tomar_del_anterior(x, i)
+            elif i < len(x.hijos) - 1 and len(x.hijos[i + 1].claves) >= t:
+                self.tomar_del_siguiente(x, i)
+            else:
+                if i < len(x.hijos):
+                    self.fusionar(x, i)
+                else:
+                    self.fusionar(x, i - 1)
+            if i == len(x.claves) and len(x.hijos) > i:
+                self.eliminar_clave(x.hijos[i], k)
+            else:
+                self.eliminar_clave(x.hijos[i + 1], k)
+        else:
+            self.eliminar_clave(x.hijos[i], k)
 
-    prev = a.e[index - 1]
-    next = a.e[index + 1]
+    def get_predecesor(self, x):
+        if x.hoja:
+            return x.claves[len(x.claves) - 1]
+        return self.get_predecesor(x.hijos[len(x.claves)])
 
-    if prev.m > n:
-        borrow_from_prev(x, a, prev, index)
-    elif next.m > n:
-        borrow_from_next(x, a, next, index)
-    else:
-        merge(a, index)
+    def get_sucesor(self, x):
+        if x.hoja:
+            return x.claves[0]
+        return self.get_sucesor(x.hijos[0])
 
-def borrow_from_prev(x, a, prev, index):
-    child = a.e[index].p
-    sibling = a.e[index - 1].p
-    child.e.insert(0, sibling.e.pop())
-    a.e[index - 1] = prev.e.pop()
-    a.e.insert(index, child.e.pop(0))
+    def tomar_del_anterior(self, x, i):
+        hijo = x.hijos[i]
+        hermano = x.hijos[i - 1]
 
-def borrow_from_next(x, a, next, index):
-    child = a.e[index].p
-    sibling = a.e[index + 1].p
-    child.e.append(next.e.pop(0))
-    a.e[index + 1] = next.e[0]
-    a.e[index] = child.e.pop()
+        hijo.claves.insert(0, x.claves[i - 1])
+        if not hijo.hoja:
+            hijo.hijos.insert(0, hermano.hijos.pop())
 
-def merge(a, index):
-    global raiz
-    if index == a.m:
-        index -= 1
-
-    child = a.e[index].p
-    sibling = a.e[index + 1].p
-    child.e.append(a.e[index])
-    child.e.extend(sibling.e)
-    a.e.pop(index + 1)
-    a.m -= 1
-
-def print_tree(node, level=0):
-    if node:
-        print("Level", level, ":", end=" ")
-        for item in node.e:
-            print(item.key, end=" ")
-        print()
-        level += 1
-        if node.e:
-            for child in node.e:
-                print_tree(child.p, level)
-
-while True:
-    print("\n\n Menú ")
-    print("\n 1_ Insertar ")
-    print("\n 2_ Suprimir ")
-    print("\n 3_ Mostrar ")
-    print("\n 4_ Salir ")
-    print("\n")
-    op = int(input("Ingrese opción: "))
-
-    if op == 1:
-        print("\n Ingrese clave a insertar (Finaliza con -1) ")
-        x = int(input())
-
-        while x >= 0:
-            insert(x, raiz)
-            print("\n Ingrese clave a insertar (Finaliza con -1) ")
-            x = int(input())
-    elif op == 2:
-        print("Ingrese clave a suprimir (Finaliza con -1) ")
-        x = int(input())
-
-        while x >= 0:
-            delete(x, raiz)
-            print("\n Ingrese clave a suprimir (Finaliza con -1) ")
-            x = int(input())
-    elif op == 3:
-        print_tree(raiz)
-    elif op == 4:
-        break
+        x.claves[i - 1] = hermano.claves.pop()
         
+    def tomar_del_siguiente(self, x, i):
+        hijo = x.hijos[i]
+        hermano = x.hijos[i + 1]
+
+        hijo.claves.append(x.claves[i])
+        if not hijo.hoja:
+            hijo.hijos.append(hermano.hijos.pop(0))
+
+        x.claves[i] = hermano.claves.pop(0)
+
+    def fusionar(self, x, i):
+        hijo = x.hijos[i]
+        hermano = x.hijos[i + 1]
+
+        hijo.claves.append(x.claves[i])
+        hijo.claves.extend(hermano.claves)
+        if not hijo.hoja:
+            hijo.hijos.extend(hermano.hijos)
+
+        x.claves.pop(i)
+        x.hijos.pop(i + 1)
+
+    def mostrar_arbol(self, x, nivel=0):
+        if x is not None:
+            print("Nivel", nivel, ":", x.claves)
+            nivel += 1
+            if not x.hoja:
+                for c in x.hijos:
+                    self.mostrar_arbol(c, nivel)
+
+def main():
+    t = int(input("Ingrese el grado mínimo del Árbol B: "))
+    arbol_b = ArbolB(t)
+
+    while True:
+        print("\nMenú:")
+        print("1. Insertar clave")
+        print("2. Suprimir clave")
+        print("3. Mostrar Árbol")
+        print("4. Salir")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == '1':
+            clave = int(input("Ingrese la clave a insertar: "))
+            arbol_b.insertar(clave)
+        elif opcion == '2':
+            clave = int(input("Ingrese la clave a suprimir: "))
+            arbol_b.eliminar(clave)
+        elif opcion == '3':
+            print("\nÁrbol B:")
+            arbol_b.mostrar_arbol(arbol_b.raiz)
+        elif opcion == '4':
+            break
+        else:
+            print("Opción no válida. Intente de nuevo.")
+
+if __name__ == "__main__":
+    main()
+            
